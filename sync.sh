1383cba2789d44548cde0354a44f656b3c370fae
Miguel <miguel_f_r_salvador@hotmail.com>
2026-01-24 21:29:23 +0000

    android backup at Sat Jan 24 21:29:23 GMT 2026

--- sync.sh
+++ sync.sh
@@ -3,8 +3,15 @@ TPWD=$PWD
 cd ~/storage/shared-external-1/obsidian/obsidian-vault
 
 git add -A
-git commit -m "android backup at $(date)"
-git pull
-git push
 
+# Commit only if there are staged changes
+if ! git diff --cached --quiet; then
+  git commit -m "android backup at $(date)"
+fi
+
+# Always pull first to receive PC updates
+git pull --rebase
+
+# Then push any phone changes
+git push
 cd $TPWD
